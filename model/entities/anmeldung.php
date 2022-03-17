6ca73a76e1f1af463d5a5ba1200e830727c8b9a8
CapBen <99887278+CapBen@users.noreply.github.com>
2022-03-17 14:18:29 +0100

    olles a wian
    getter setter erstellen

--- model/entities/anmeldung.php
+++ model/entities/anmeldung.php
@@ -21,6 +21,78 @@ class Anmeldung{
 
 
 
+    public function getToken(){
+        return $this->token;
+    }
+    public function setToken($token){
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function getDatum(){
+        return $this->datum;
+    }
+    public function setDatum($datum){
+        $this->datum = $datum;
+
+        return $this;
+    }
+
+    public function getTelefon(){
+        return $this->telefon;
+    }
+    public function setTelefon($telefon){
+        $this->telefon = $telefon;
+
+        return $this;
+    }
+
+    public function getVorname(){
+        return $this->vorname;
+    }
+    public function setVorname($vorname){
+        $this->vorname = $vorname;
+
+        return $this;
+    }
+
+    public function getNachname()
+    {
+        return $this->nachname;
+    }
+    public function setNachname($nachname){
+        $this->nachname = $nachname;
+
+        return $this;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+    public function setEmail($email){
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getFuehrung_id(){
+        return $this->fuehrung_id;
+    }
+    public function setFuehrung_id($fuehrung_id){
+        $this->fuehrung_id = $fuehrung_id;
+
+        return $this;
+    }
+
+    public function getAnzahl(){
+        return $this->anzahl;
+    }
+    public function setAnzahl($anzahl){
+        $this->anzahl = $anzahl;
+
+        return $this;
+    }
 }
 
 
