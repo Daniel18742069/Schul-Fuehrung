a687b5c1534132b8b03b71facfceca0de4615378
schweigkoflernils <83164328+schweigkoflernils@users.noreply.github.com>
2022-03-21 15:03:35 +0100

    :+1:
    :100: :100: :100:

--- view/startseite.tpl.php
+++ view/startseite.tpl.php
@@ -66,7 +66,15 @@
 
         <footer>
             <!-- map -->
-            <div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=39100,%20Romstra%C3%9Fe%2020&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><a href="https://www.embedgooglemap.net">how to add google map</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div>
+            <div class="mapouter">
+                <div class="gmap_canvas">
+                    <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=39100,%20Romstra%C3%9Fe%2020&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                    </iframe>
+                    <a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/"></a>
+                    <br>
+                    <a href="https://www.embedgooglemap.net">how to add google map</a>
+                    </div>
+                </div>
         </footer>
 
     </body>
