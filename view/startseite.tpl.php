1ff607b3ef53b4a38a1cd241eaabce21f3de1892
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2022-03-23 09:25:52 +0100

    Tabs gefixed

--- view/startseite.tpl.php
+++ view/startseite.tpl.php
@@ -35,74 +35,101 @@
 
 
         <div class="box">
-        <!-- Tabs Fachrichtung -->
-        <div class="navbar-wrapper">
-            <button class="info_elektro-button" onclick="openCalender('info_elektro')">Informatik / Elektrotechnik</button>
-            <button class="elektro_mechatronik-button" onclick="openCalender('elektro_mechatronik')">Elektrotechnik / Mechatronik</button>
-            <button class="friseur-button" onclick="openCalender('friseur')">Friseur</button>
-            <button class="holzbau-button" onclick="openCalender('holzbau')">Holzbau</button>
-        </div>
-        <body>
-
-
-            <!-- Kalender -->
-            <div>
-                <div id="info_elektro" class="fachrichtung">
-                    <h2>Informatik / Elektrotechnik</h2>
-                    <p>TEST TEST Info Elektro</p>
-                </div>
-
-        <!-- Kalender -->
-        <div>
-            <div id="info_elektro" class="fachrichtung">
-                <h2>Informatik / Elektrotechnik</h2>
-                <?php $counter = 8;
-                for ($i = 0; $i <= 10; $i++){ ?>
-                <div class="kalenderbox">
-                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
-                    <span class="lehrer">Lehrer</span>
-                    <span class="kapazitaet">0/10</span>
-                </div>
-                <?php $counter++; } ?>
+            <!-- Tabs Fachrichtung -->
+            <div class="navbar-wrapper">
+                <button class="info_elektro-button" onclick="openCalender('info_elektro')">Informatik / Elektrotechnik</button>
+                <button class="elektro_mechatronik-button" onclick="openCalender('elektro_mechatronik')">Elektrotechnik / Mechatronik</button>
+                <button class="friseur-button" onclick="openCalender('friseur')">Friseur</button>
+                <button class="holzbau-button" onclick="openCalender('holzbau')">Holzbau</button>
             </div>
 
-            <div id="elektro_mechatronik" class="fachrichtung" style="display:none">
-                <h2>Elektrotechnik / Mechatronik</h2>
-                <?php $counter = 8;
-                for ($i = 0; $i <= 10; $i++){ ?>
-                <div class="kalenderbox">
-                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
-                    <span class="lehrer">Lehrer</span>
-                    <span class="kapazitaet">0/10</span>
-                </div>
-                <?php $counter++; } ?>
-            </div>
+            <body>
 
-            <div id="friseur" class="fachrichtung" style="display:none">
-                <h2>Friseur</h2>
-                <?php $counter = 8;
-                for ($i = 0; $i <= 10; $i++){ ?>
-                <div class="kalenderbox">
-                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
-                    <span class="lehrer">Lehrer</span>
-                    <span class="kapazitaet">0/10</span>
-                </div>
-                <?php $counter++; } ?>
-            </div>
+                <!-- Tabs Fachrichtung -->
+                <div class="tabs">
+                    <input type="button" name="tabs" id="tab_elektro" checked="checked" class="info_elektro-button" onclick="openCalender('info_elektro')">
+                    <label for="tab_elektro">Informatik / Elektrotechnik</label>
+                    <div class="tab">
+                    </div>
 
-            <div id="holzbau" class="fachrichtung" style="display:none">
-                <h2>Holzbau</h2>
-                <?php $counter = 8;
-                for ($i = 0; $i <= 10; $i++){ ?>
-                <div class="kalenderbox">
-                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
-                    <span class="lehrer">Lehrer</span>
-                    <span class="kapazitaet">0/10</span>
+                    <input type="button" name="tabs" id="tab_two" class="elektro_mechatronik-button" onclick="openCalender('elektro_mechatronik')">
+                    <label for="tab_two">Elektrotechnik / Mechatronik</label>
+                    <div class="tab">
+                    </div>
+
+                    <input type="button" name="tabs" id="tab_three" class="friseur-button" onclick="openCalender('friseur')">
+                    <label for="tab_three">Friseur</label>
+                    <div class="tab">
+                    </div>
+
+                <input type="button" name="tabs" id="tab_four" class="holzbau-button" onclick="openCalender('holzbau')">
+                    <label for="tab_four">Holzbau</label>
+                    <div class="tab">
+                    </div>
                 </div>
-                <?php $counter++; } ?>
-            </div>
-        </div>
-    </div>
+
+                <!-- Kalender -->
+                <div>
+                    <div id="info_elektro" class="fachrichtung">
+                        <h2>Informatik / Elektrotechnik</h2>
+                        <p>TEST TEST Info Elektro</p>
+                    </div>
+
+                    <!-- Kalender -->
+                    <div>
+                        <div id="info_elektro" class="fachrichtung">
+                            <h2>Informatik / Elektrotechnik</h2>
+                            <?php $counter = 8;
+                            for ($i = 0; $i <= 10; $i++) { ?>
+                                <div class="kalenderbox">
+                                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
+                                    <span class="lehrer">Lehrer</span>
+                                    <span class="kapazitaet">0/10</span>
+                                </div>
+                            <?php $counter++;
+                            } ?>
+                        </div>
+
+                        <div id="elektro_mechatronik" class="fachrichtung" style="display:none">
+                            <h2>Elektrotechnik / Mechatronik</h2>
+                            <?php $counter = 8;
+                            for ($i = 0; $i <= 10; $i++) { ?>
+                                <div class="kalenderbox">
+                                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
+                                    <span class="lehrer">Lehrer</span>
+                                    <span class="kapazitaet">0/10</span>
+                                </div>
+                            <?php $counter++;
+                            } ?>
+                        </div>
+
+                        <div id="friseur" class="fachrichtung" style="display:none">
+                            <h2>Friseur</h2>
+                            <?php $counter = 8;
+                            for ($i = 0; $i <= 10; $i++) { ?>
+                                <div class="kalenderbox">
+                                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
+                                    <span class="lehrer">Lehrer</span>
+                                    <span class="kapazitaet">0/10</span>
+                                </div>
+                            <?php $counter++;
+                            } ?>
+                        </div>
+
+                        <div id="holzbau" class="fachrichtung" style="display:none">
+                            <h2>Holzbau</h2>
+                            <?php $counter = 8;
+                            for ($i = 0; $i <= 10; $i++) { ?>
+                                <div class="kalenderbox">
+                                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
+                                    <span class="lehrer">Lehrer</span>
+                                    <span class="kapazitaet">0/10</span>
+                                </div>
+                            <?php $counter++;
+                            } ?>
+                        </div>
+                    </div>
+                </div>
                 <!-- Untertitel & kurzer Text -->
                 <div>
                     <h2>Zeit für einen Rundgang?</h2>
@@ -112,44 +139,16 @@
                 </div>
 
 
-                <!-- Tabs Fachrichtung -->
-                <div class="tabs">
-                    <input type="radio" name="tabs" id="tab_one" checked="checked">
-                    <label for="tab_one">Tab One</label>
-                    <div class="tab">
-                        <h1>Tab One Content</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <footer>
+                    <!-- map -->
+                    <div class="mapouter">
+                        <div class="gmap_canvas">
+                            <iframe width="425" height="160" id="gmap_canvas" src="https://maps.google.com/maps?q=39100,%20Romstra%C3%9Fe%2020&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                            </iframe>
+                        </div>
                     </div>
 
-                    <input type="radio" name="tabs" id="tab_two">
-                    <label for="tab_two">Tab Two</label>
-                    <div class="tab">
-                        <h1>Tab Two Content</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-
-                    <input type="radio" name="tabs" id="tab_three">
-                    <label for="tab_three">Tab Three</label>
-                    <div class="tab">
-                        <h1>Tab Three Content</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                    </div>
-                </div>
-
-
-
-
-        <footer>
-            <!-- map -->
-            <div class="mapouter">
-                <div class="gmap_canvas">
-                    <iframe width="425" height="160" id="gmap_canvas" src="https://maps.google.com/maps?q=39100,%20Romstra%C3%9Fe%2020&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-                    </iframe>
-                    </div>
-                </div>
-
-        </body>
+            </body>
 
 
     </section>
