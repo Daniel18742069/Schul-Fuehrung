97fed480f9827e412338a0c00a92c139dc3b2263
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2022-03-28 09:36:00 +0200

    Active Tabs [FAILED] 2

--- model/JS/script.js
+++ model/JS/script.js
@@ -16,3 +16,24 @@ var mapOptions = {
     center: new google.maps.LatLng(51.5, -0.2), zoom: 10
 }
 var map = new google.maps.Map(mapCanvas, mapOptions);
+
+
+
+//tabs
+function rudrSwitchTab(rudr_tab_id, rudr_tab_content) {
+    // first of all we get all tab content blocks (I think the best way to get them by class names)
+    var x = document.getElementsByClassName("fachrichtung");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'none'; // hide all tab content
+    }
+    document.getElementById(rudr_tab_content).style.display = 'block'; // display the content of the tab we need
+
+    // now we get all tab menu items by class names (use the next code only if you need to highlight current tab)
+    var x = document.getElementsByClassName("tab");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].className = 'tab';
+    }
+    document.getElementById(rudr_tab_id).className = 'tab active';
+}
--- view/startseite.tpl.php
+++ view/startseite.tpl.php
@@ -39,107 +39,87 @@
 
                 <!-- Tabs Fachrichtung -->
                 <div class="tabs" class="active">
-                    <input type="button" name="tabs" data-tab-target="#info_elektro" id="tab_elektro" checked="checked" class="info_elektro-button active tab" onclick="openCalender('info_elektro')">
+                    <input type="button" href="javascript:rudrSwitchTab('tab_elektro', 'content_1');" name="tabs" id="tab_elektro" checked="checked" class="info_elektro-button active tab" onclick="openCalender('info_elektro')">
                     <label for="tab_elektro">Informatik / Elektrotechnik</label>
-                   
-                    <input type="button" name="tabs" data-tab-target="#elektro_mechatronik" id="tab_mechatronik" class="elektro_mechatronik-button tab" onclick="openCalender('elektro_mechatronik')">
+
+                    <input type="button" href="javascript:rudrSwitchTab('tab_mechatronik', 'content_2');" name="tabs" id="tab_mechatronik" class="elektro_mechatronik-button tab" onclick="openCalender('elektro_mechatronik')">
                     <label for="tab_mechatronik">Elektrotechnik / Mechatronik</label>
-            
-                    <input type="button" name="tabs" data-tab-target="#friseur" id="tab_friseur" class="friseur-button tab" onclick="openCalender('friseur')">
+
+                    <input type="button" href="javascript:rudrSwitchTab('tab_friseur', 'content_3');" name="tabs" id="tab_friseur" class="friseur-button tab" onclick="openCalender('friseur')">
                     <label for="tab_friseur">Friseur</label>
-                 
-                    <input type="button" name="tabs" data-tab-target="#holzbau" id="tab_holz" class="holzbau-button tab" onclick="openCalender('holzbau')">
+
+                    <input type="button" href="javascript:rudrSwitchTab('tab_holz', 'content_4');" name="tabs" id="tab_holz" class="holzbau-button tab" onclick="openCalender('holzbau')">
                     <label for="tab_holz">Holzbau</label>
                 </div>
 
-                    <script>
-                    const tabs = document.querySelectorAll('[data-tab-target]')
-                    const tabContents = document.querySelectorAll('[data-tab-content]')
+            </div>
 
-                    tabs.forEach(tab => {
-                        tab.addEventListener('click', () => {
-                            const target = document.querySelector(tab.dataset.tabTarget)
-                            tabContents.forEach(tabContent => {
-                                tabContent.classList.remove('active')
-                            })
-                            tabs.forEach(tab => {
-                                tab.classList.remove('active')
-                            })
-                            tab.classList.add('active')
-                            target.classList.add('active')
-                        })
-                    })
-                </script>
-
-                </div>
-
+            <!-- Kalender -->
+            <div class="tab-content">
                 <!-- Kalender -->
-                <div class="tab-content">
-                    <!-- Kalender -->
-                    <div>
-                        <div id="info_elektro" data-tab-content class="fachrichtung active">
-                            <h2>Informatik / Elektrotechnik</h2>
-                            <?php $counter = 8;
-                            for ($i = 0; $i <= 10; $i++) { ?>
-                                <div class="kalenderbox">
-                                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
-                                    <span class="lehrer">Lehrer</span>
-                                    <span class="kapazitaet">0/10</span>
-                                </div>
-                            <?php $counter++;
-                            } ?>
-                        </div>
+                <div>
+                    <div id="info_elektro" data-tab-content class="fachrichtung active">
+                        <h2>Informatik / Elektrotechnik</h2>
+                        <?php $counter = 8;
+                        for ($i = 0; $i <= 10; $i++) { ?>
+                            <div class="kalenderbox">
+                                <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
+                                <span class="lehrer">Lehrer</span>
+                                <span class="kapazitaet">0/10</span>
+                            </div>
+                        <?php $counter++;
+                        } ?>
+                    </div>
 
-                        <div id="elektro_mechatronik" data-tab-content class="fachrichtung" style="display:none">
-                            <h2>Elektrotechnik / Mechatronik</h2>
-                            <?php $counter = 8;
-                            for ($i = 0; $i <= 10; $i++) { ?>
-                                <div class="kalenderbox">
-                                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
-                                    <span class="lehrer">Lehrer</span>
-                                    <span class="kapazitaet">0/10</span>
-                                </div>
-                            <?php $counter++;
-                            } ?>
-                        </div>
+                    <div id="elektro_mechatronik" data-tab-content class="fachrichtung" style="display:none">
+                        <h2>Elektrotechnik / Mechatronik</h2>
+                        <?php $counter = 8;
+                        for ($i = 0; $i <= 10; $i++) { ?>
+                            <div class="kalenderbox">
+                                <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
+                                <span class="lehrer">Lehrer</span>
+                                <span class="kapazitaet">0/10</span>
+                            </div>
+                        <?php $counter++;
+                        } ?>
+                    </div>
 
-                        <div id="friseur" data-tab-content class="fachrichtung" style="display:none">
-                            <h2>Friseur</h2>
-                            <?php $counter = 8;
-                            for ($i = 0; $i <= 10; $i++) { ?>
-                                <div class="kalenderbox">
-                                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
-                                    <span class="lehrer">Lehrer</span>
-                                    <span class="kapazitaet">0/10</span>
-                                </div>
-                            <?php $counter++;
-                            } ?>
-                        </div>
+                    <div id="friseur" data-tab-content class="fachrichtung" style="display:none">
+                        <h2>Friseur</h2>
+                        <?php $counter = 8;
+                        for ($i = 0; $i <= 10; $i++) { ?>
+                            <div class="kalenderbox">
+                                <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
+                                <span class="lehrer">Lehrer</span>
+                                <span class="kapazitaet">0/10</span>
+                            </div>
+                        <?php $counter++;
+                        } ?>
+                    </div>
 
-                        <div id="holzbau" data-tab-content class="fachrichtung" style="display:none">
-                            <h2>Holzbau</h2>
-                            <?php $counter = 8;
-                            for ($i = 0; $i <= 10; $i++) { ?>
-                                <div class="kalenderbox">
-                                    <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
-                                    <span class="lehrer">Lehrer</span>
-                                    <span class="kapazitaet">0/10</span>
-                                </div>
-                            <?php $counter++;
-                            } ?>
-                        </div>
+                    <div id="holzbau" data-tab-content class="fachrichtung" style="display:none">
+                        <h2>Holzbau</h2>
+                        <?php $counter = 8;
+                        for ($i = 0; $i <= 10; $i++) { ?>
+                            <div class="kalenderbox">
+                                <span class="uhrzeit"><?= $counter ?>:00 Uhr</span>
+                                <span class="lehrer">Lehrer</span>
+                                <span class="kapazitaet">0/10</span>
+                            </div>
+                        <?php $counter++;
+                        } ?>
                     </div>
                 </div>
+            </div>
 
-
-                <footer>
-                    <!-- map -->
-                    <div class="mapouter">
-                        <div class="gmap_canvas">
-                            <iframe width="425" height="160" id="gmap_canvas" src="https://maps.google.com/maps?q=39100,%20Romstra%C3%9Fe%2020&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-                            </iframe>
-                        </div>
+            <footer>
+                <!-- map -->
+                <div class="mapouter">
+                    <div class="gmap_canvas">
+                        <iframe width="425" height="160" id="gmap_canvas" src="https://maps.google.com/maps?q=39100,%20Romstra%C3%9Fe%2020&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                        </iframe>
                     </div>
+                </div>
 
         </body>
 
