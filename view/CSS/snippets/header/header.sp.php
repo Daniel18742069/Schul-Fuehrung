4944ca6dc8572b5bc86018843b40ffc6eae732b0
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2022-03-30 11:30:08 +0200

    Snippets Header und Footer eingefügt

--- view/CSS/snippets/header/header.sp.php
+++ view/CSS/snippets/header/header.sp.php
@@ -1,4 +1,14 @@
 <header class="header">
+    <style>
+        .header {
+            text-align: center;
+            border: 2px solid #002a50;
+            padding-top: 2em;
+        }
+            #logo {
+                margin: 2em;
+            }
+    </style>
     <div id="logo">
         <img src="CSS\img\Logos Berufsschule.bz\LBSHI_Logo_RGB_PNG.png" alt="Landesberufsschule für Handwerk und Industrie Bozen" width="369" height="80" loading="lazy"></a>
     </div>
