979431e5b3b2a19d2b2f7d7a12f0cada59d83062
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2022-04-02 22:05:03 +0200

    An der responsiveness herumgeschreubt [FAILED]

--- view/Frontend/template/startseite.tpl.php
+++ view/Frontend/template/startseite.tpl.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Schulführung</title>
     <link rel="stylesheet" href="../CSS/style_startseite.css" />
     <link rel="stylesheet" href="../CSS/style_header.css" />
