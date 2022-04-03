728f8b9681b4c3efc66a43afb1595671c9651183
schweigkoflernils <83164328+schweigkoflernils@users.noreply.github.com>
2022-04-03 21:34:02 +0200

    FOOTER FERTIG

--- view/Frontend/snippets/xfooter.sp.php
+++ view/Frontend/snippets/xfooter.sp.php
@@ -4,35 +4,48 @@
     <div class="footer-box">
         <span class="înfos-schule">
             <h3>Landesberfusschule für Handwerk und Industrie Bozen</h3>
-            <span>
-                <p>Romstraße 20</p>
-                <p>39100 Bozen</p>
-                <p>Tel.: <a href="tel:+39 0471 540700">+39 0471 540700</a></p>
-                <h5>Emails:</h5>
-                <p><a href="mailto:lbs.bz-handwerk-industrie@schule.suedtirol.it">lbs.bz-handwerk-industrie@schule.suedtirol.it</a></p>
-                <p><a href="mailto:lbs.bz-handwerk-industrie@pec.prov.bz.it">lbs.bz-handwerk-industrie@pec.prov.bz.it</a></p>
-            </span>
+            <p>Romstraße 20</p>
+            <p>39100 Bozen</p>
+            <p>Tel.: <a href="tel:+39 0471 540700">+39 0471 540700</a></p>
+            <h5>Emails:</h5>
+            <p><a href="mailto:lbs.bz-handwerk-industrie@schule.suedtirol.it">lbs.bz-handwerk-industrie@schule.suedtirol.it</a></p>
+            <p><a href="mailto:lbs.bz-handwerk-industrie@pec.prov.bz.it">lbs.bz-handwerk-industrie@pec.prov.bz.it</a></p>
         </span>
         <span class="oeffnungszeiten">
             <h3>Öffnungszeiten</h3>
             <h5>Sekretariat</h5>
-            <span>
-                <p>Parteienverkehr nur mit Terminvereinbarung</p>
-                <p>Mo - Fr 8:30-16:30 Uhr</p>
-                <p>Tel.: <a href="tel:+39 0471 540700">+39 0471 540700</a></p>
-                <h5>Emails:</h5>
-                <p><a href="mailto:lbs.bz-handwerk-industrie@schule.suedtirol.it">lbs.bz-handwerk-industrie@schule.suedtirol.it</a></p>
-                <p><a href="mailto:lbs.bz-handwerk-industrie@pec.prov.bz.it">lbs.bz-handwerk-industrie@pec.prov.bz.it</a></p>
-            </span>
+            <p>Parteienverkehr nur mit Terminvereinbarung</p>
+            <p>Mo - Fr 8:30-16:30 Uhr</p>
+            <p>Tel.: <a href="tel:+39 0471 540700">+39 0471 540700</a></p>
+            <h5>Emails:</h5>
+            <p><a href="mailto:lbs.bz-handwerk-industrie@schule.suedtirol.it">lbs.bz-handwerk-industrie@schule.suedtirol.it</a></p>
+            <p><a href="mailto:lbs.bz-handwerk-industrie@pec.prov.bz.it">lbs.bz-handwerk-industrie@pec.prov.bz.it</a></p>
         </span>
-        <span class="socials"></span>
-        <!-- map -->
-        <div class="mapouter">
-            <div class="gmap_canvas">
-                <iframe width="425" height="160" id="gmap_canvas" src="https://maps.google.com/maps?q=39100,%20Romstra%C3%9Fe%2020&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-                </iframe>
+        <span class="socials">
+            <h3>Finde uns auf</h3>
+            <div class="social_icons-footer">
+
+                <a class="btn btn-social-icon" href="https://www.facebook.com/lbs.bozen" title="Facebook">
+                    <i class="fa fa-facebook-square fa-2x icon-cog" aria-hidden="true"></i>
+                    <span class="sr-only">Facebook</span></a>
+
+                <a class="btn btn-social-icon" href="https://www.instagram.com/lbshandwerkindustrie/" title="Instagram">
+                    <i class="fa fa-instagram fa-2x icon-cog" aria-hidden="true"> </i>
+                    <span class="sr-only">Instagram</span></a>
+
+                <a class="btn btn-social-icon" href="https://www.youtube.com/channel/UCsAU2KSrwCFmkUFeCMhQD4Q/feed" title="YouTube">
+                    <i class="fa fa-youtube fa-2x icon-cog" aria-hidden="true"> </i>
+                    <span class="sr-only">YouTube</span></a>
+
             </div>
-        </div>
+
+            <div class="mapouter">
+                <div class="gmap_canvas">
+                    <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=39100,%20Romstra%C3%9Fe%2020&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                    </iframe>
+                </div>
+            </div>
+        </span>
     </div>
 </div>
 </footer>
