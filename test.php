bbb206fff46beb27e7331310320f90d19d082ee1
schweigkoflernils <83164328+schweigkoflernils@users.noreply.github.com>
2022-04-27 10:52:22 +0200

    date

--- test.php
+++ test.php
@@ -2,15 +2,11 @@
 
 $heute = new DateTime("10:30");
 
-
-
-var_dump($heute);
-
-echo "<br>";
-
 $minutes_to_add = 60;
 
 $temp = $heute->add(new DateInterval('PT' . $minutes_to_add . 'M'));
 
-//var_dump($temp->);
+echo date_format($temp, 'H:i');
+
+
 ?>
