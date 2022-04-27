55bdce8a8fd6fb3d7bb7ca00db3bb6c7a27ec9ea
schweigkoflernils <83164328+schweigkoflernils@users.noreply.github.com>
2022-04-27 10:46:01 +0200

    to main

--- test.php
+++ test.php
@@ -1,6 +1,16 @@
 <?php
 
-$startzeit = strtotime("10:00");
-echo $startzeit;
+$heute = new DateTime("10:30");
 
+
+
+var_dump($heute);
+
+echo "<br>";
+
+$minutes_to_add = 60;
+
+$temp = $heute->add(new DateInterval('PT' . $minutes_to_add . 'M'));
+
+//var_dump($temp->);
 ?>
