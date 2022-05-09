7429709e64f64f87cf4cc0b53a528a413b0e936d
Andreas Codalonga <45843565+TheArled@users.noreply.github.com>
2022-05-09 14:34:53 +0200

    moved default site setting to settings

--- index.php
+++ index.php
@@ -12,23 +12,18 @@ require_once 'model/entities/offener_tag.php';
 require_once 'controller/controller.php';
 
 session_start();
-$aktion = isset($_GET['aktion'])?$_GET['aktion']:'be_login_admin';
-
-
-//$aktion = isset($_GET['aktion'])?$_GET['aktion']:'fe_startseite';
-//logge_aus();
-
+$aktion = isset($_GET['aktion']) ? $_GET['aktion'] : CONF['DEFAULT_SITE'];
 
 $controller = new Controller();
 
-if(substr($aktion,0,2) == "be"){
-    if($aktion == "be_login_admin"){
+if (substr($aktion, 0, 2) == "be") {
+    if ($aktion == "be_login_admin") {
         // Platzhalter
-    }else if(!ist_eingeloggt()){
+    } else if (!ist_eingeloggt()) {
         $aktion = "be_login_admin";
     }
 }
 if (method_exists($controller, $aktion)) {
-     
+
     $controller->run($aktion);
 }
