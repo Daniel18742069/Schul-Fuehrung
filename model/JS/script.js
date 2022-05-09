f812dc6fbb6c8c9316e4185f21fd40662aa924b7
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2022-05-09 14:17:10 +0200

    In schinken va lests mol

--- model/JS/script.js
+++ model/JS/script.js
@@ -116,3 +116,74 @@ function hideShowElement(source, target) {
 //XML Tabelle sortieren
 
 
+function table_sort() {
+    const styleSheet = document.createElement('style')
+    styleSheet.innerHTML = `
+      .order-inactive span {
+          visibility:hidden;
+      }
+      .order-inactive:hover span {
+          visibility:visible;
+      }
+      .order-active span {
+          visibility: visible;
+      }
+  `
+    document.head.appendChild(styleSheet)
+
+    document.querySelectorAll('th.order').forEach(th_elem => {
+        let asc = true
+        const span_elem = document.createElement('span')
+        span_elem.style = "font-size:0.8rem; margin-left:0.5rem"
+        span_elem.innerHTML = "▼"
+        th_elem.appendChild(span_elem)
+        th_elem.classList.add('order-inactive')
+
+        const index = Array.from(th_elem.parentNode.children).indexOf(th_elem)
+        th_elem.addEventListener('click', (e) => {
+            document.querySelectorAll('th.order').forEach(elem => {
+                elem.classList.remove('order-active')
+                elem.classList.add('order-inactive')
+            })
+            th_elem.classList.remove('order-inactive')
+            th_elem.classList.add('order-active')
+
+            if (!asc) {
+                th_elem.querySelector('span').innerHTML = '▲'
+            } else {
+                th_elem.querySelector('span').innerHTML = '▼'
+            }
+            const arr = Array.from(th_elem.closest("table").querySelectorAll('tbody tr'))
+            arr.sort((a, b) => {
+                const a_val = a.children[index].innerText
+                const b_val = b.children[index].innerText
+                return (asc) ? a_val.localeCompare(b_val) : b_val.localeCompare(a_val)
+            })
+            arr.forEach(elem => {
+                th_elem.closest("table").querySelector("tbody").appendChild(elem)
+            })
+            asc = !asc
+        })
+    })
+}
+
+
+$(function() {
+    $(".exportToExcel").click(function(e){
+        var table = $(this).prev('.table2excel');
+        if(table && table.length){
+            var preserveColors = (table.hasClass('table2excel_with_colors') ? true : false);
+            $(table).table2excel({
+                exclude: ".noExl",
+                name: "Excel Document Name",
+                filename: "myFileName" + new Date().toISOString().replace(/[\-\:\.]/g, "") + ".xls",
+                fileext: ".xls",
+                exclude_img: true,
+                exclude_links: true,
+                exclude_inputs: true,
+                preserveColors: preserveColors
+            });
+        }
+    });
+    
+});
--- model/printXLS.php
+++ model/printXLS.php
@@ -1,15 +1,8 @@
-<script src=
-"https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js">
-        </script>
-        <script src="jquery.table2excel.js"></script>
 <?php
 
-function cleanData(&$str)
-{
-    $str = preg_replace("/\t/", "\\t", $str);
-    $str = preg_replace("/\r?\n/", "\\n", $str);
-    if (strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
-}
+require_once 'view/template/printXLS.tpl.php';
+
+
 
 // filename for download
 $filename = "open_day_" . date('Y.m.d') . ".xls";
@@ -27,66 +20,7 @@ $alleFuehrungen = Fuehrung::findeAlleFuehrungen();
 if ($alleAnmeldungen && $alleFachrichtungen && $alleFuehrungen) {
 ?>
 
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Jost:wght@100;300;400;700&display=swap');
-
-        @media print {
-            * {
-                display: none;
-            }
-
-            #printableTable {
-                display: block;
-            }
-        }
-
-        table,
-        th,
-        td {
-            border: .5px solid black;
-        }
-
-        table td:first-child::first-letter {
-            text-transform: uppercase;
-        }
-
-        table {
-            font-size: 12px;
-            color: #333333;
-            width: 100%;
-            border-width: 1px;
-            border-color: #a9a9a9;
-            border-collapse: collapse;
-        }
-
-        table thead {
-            cursor: pointer;
-        }
-
-        table th {
-            font-size: 12px;
-            background-color: #b8b8b8;
-            border-width: 1px;
-            padding: 8px;
-            border-style: solid;
-            border-color: #a9a9a9;
-            text-align: left;
-        }
-
-        table td {
-            font-size: 12px;
-            border-width: 1px;
-            padding: 8px;
-            border-style: solid;
-            border-color: #a9a9a9;
-        }
-
-        table tr:hover {
-            background-color: #ffff99;
-        }
-    </style>
-
-    <table id="table">
+    <table class="table" id="table2excel">
         <thead>
             <tr>
                 <th class="order">Datum</th>
@@ -148,102 +82,14 @@ if ($alleAnmeldungen && $alleFachrichtungen && $alleFuehrungen) {
             <?php endforeach; ?>
         </tbody>
     </table>
-    <button class="exportBtnClass">
-                  Export to XLS file
-         </button>
-    <input type="hidden" name="file_content" id="file_content" />
-    <button type="button" name="convert" id="convert" class="btn btn-primary">Convert</button>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <button class="exportToExcel">Export</button>
+    <script>
+        table_sort()
+    </script>
+
 <?php
 
 }
-
-?>
-
-
-<script>
-    function table_sort() {
-        const styleSheet = document.createElement('style')
-        styleSheet.innerHTML = `
-          .order-inactive span {
-              visibility:hidden;
-          }
-          .order-inactive:hover span {
-              visibility:visible;
-          }
-          .order-active span {
-              visibility: visible;
-          }
-      `
-        document.head.appendChild(styleSheet)
-
-        document.querySelectorAll('th.order').forEach(th_elem => {
-            let asc = true
-            const span_elem = document.createElement('span')
-            span_elem.style = "font-size:0.8rem; margin-left:0.5rem"
-            span_elem.innerHTML = "▼"
-            th_elem.appendChild(span_elem)
-            th_elem.classList.add('order-inactive')
-
-            const index = Array.from(th_elem.parentNode.children).indexOf(th_elem)
-            th_elem.addEventListener('click', (e) => {
-                document.querySelectorAll('th.order').forEach(elem => {
-                    elem.classList.remove('order-active')
-                    elem.classList.add('order-inactive')
-                })
-                th_elem.classList.remove('order-inactive')
-                th_elem.classList.add('order-active')
-
-                if (!asc) {
-                    th_elem.querySelector('span').innerHTML = '▲'
-                } else {
-                    th_elem.querySelector('span').innerHTML = '▼'
-                }
-                const arr = Array.from(th_elem.closest("table").querySelectorAll('tbody tr'))
-                arr.sort((a, b) => {
-                    const a_val = a.children[index].innerText
-                    const b_val = b.children[index].innerText
-                    return (asc) ? a_val.localeCompare(b_val) : b_val.localeCompare(a_val)
-                })
-                arr.forEach(elem => {
-                    th_elem.closest("table").querySelector("tbody").appendChild(elem)
-                })
-                asc = !asc
-            })
-        })
-    }
-    table_sort()
-</script>
-
-<script>
-    $(function() {
-                  
-                  $(".exportBtnClass").click(function(e){
-                      var table = $(this).prev('.table2excel');
-                      if(table && table.length){
-                          var preserveColors = 
-                         (table.hasClass('colorClass') ? true : false);
-    
-                          $(table).table2excel({
-    
-  // This class's content is excluded from getting exported
-                              exclude: ".noExl", 
-                              name: "Output excel file ",
-                              filename: 
-  "outputFile-" + new Date().toString().replace(/[\-\:\.]/g, "") + ".xls",
-    
-                              fileext: ".xls", //File extension type
-                              exclude_img: true,
-                              exclude_links: true,
-                              exclude_inputs: true,
-                              preserveColors: preserveColors
-                          });
-                      }
-                  });        
-              });
-</script>
-
-<?php
 
 function cleanUmlaute(string $string)
 {
@@ -253,4 +99,14 @@ function cleanUmlaute(string $string)
     return str_replace($search, $replace, $string);
 }
 
+/*function cleanData(&$str)
+{
+    $str = preg_replace("/\t/", "\\t", $str);
+    $str = preg_replace("/\r?\n/", "\\n", $str);
+    if (strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
+}
+*/
+
 exit;
+
+?>
