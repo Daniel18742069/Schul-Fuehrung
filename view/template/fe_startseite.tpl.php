cc6e9f625f8c118daca28107be3f3c36038d1392
Andreas Codalonga <45843565+TheArled@users.noreply.github.com>
2022-05-17 14:57:25 +0200

    Revert "Merge branch 'main' into startseite_fronend_refactoring"
    This reverts commit 5cbf9837f3ce85de3a4b7546a24b52de1c666694, reversing
    changes made to ca3d52d083d748db947f43f2d7ebf693776ff68d.

--- view/template/fe_startseite.tpl.php
+++ view/template/fe_startseite.tpl.php
@@ -77,23 +77,12 @@
 
 
                 <div class="accordion js-accordion" id="accordion">
-                    <?php
-                    $fuehrungen = Fuehrung::findeSpezifischeFuehrungen($offener_tag->getId(), 2); //I DONT KNOW
-                    //$intervall = $offener_tag->getIntervall() * 60;
-                    //for ($seconds = 0; $seconds <= $offener_tag->getEnde(); $seconds + (60)) {
-                    foreach ($fuehrungen as $fuehrung) {
-                    ?>
+                    <?php foreach ($fuehrungen as $fuehrung) { ?>
                         <div class="accordion__item js-accordion-item fuehrung <?= $fuehrung->getFachrichtung_id(); ?>">
                             <div class="accordion-header js-accordion-header">
-                                <?php
-                                $anzahlTeilnehmer =  Anmeldung::anzahlTeilnehmer($fuehrung->getId());
-                                if ($anzahlTeilnehmer == NULL) {
-                                    $anzahlTeilnehmer = 0;
-                                }
-                                ?>
                                 <div class="uhrzeit"><?= $fuehrung->getUhrzeitWelformed(); ?> Uhr</div>
                                 <div class="lehrer"><?= $fuehrung->getFuehrungspersonen(); ?></div>
-                                <div class="kapazitaet"><?= $anzahlTeilnehmer ?>/<?= $fuehrung->getKapazitaet(); ?></div>
+                                <div class="kapazitaet"><?= $anzahl_teilnehmer[$fuehrung->getId()] ?>/<?= $fuehrung->getKapazitaet(); ?></div>
                             </div>
                             <div class="accordion-body js-accordion-body">
                                 <div class="accordion-body__contents">
@@ -157,10 +146,10 @@
                     id: 'captcha',
                     repeatIcon: 'fa fa-redo',
                     onSuccess: function() {
-                        showHideCaptcha();
-                        var handler = setTimeout(function() {
+                        var handler = setTimeout(() => {
                             window.clearTimeout(handler);
                             captcha.reset();
+                            showHideCaptcha();
                         }, 500);
                     }
                 });
