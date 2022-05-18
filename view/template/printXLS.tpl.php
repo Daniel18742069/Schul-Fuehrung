726bbd982a72b220716bc75f0b6e9ae53903b475
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2022-05-18 11:21:25 +0200

    Tabellen fertig

--- view/template/printXLS.tpl.php
+++ view/template/printXLS.tpl.php
@@ -5,13 +5,13 @@
     <title>OPEN DAY</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.5.1.js" type="text/javascript"></script>
-    <script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js" type="text/javascript"></script>
-    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js" type="text/javascript"></script>
-    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js" type="text/javascript"></script>
+    <script src="plugin/js/jquery-3.5.1.js" type="text/javascript"></script>
+    <script src="plugin/js/jquery.dataTables.min.js" type="text/javascript"></script>
+    <script src="plugin/js/dataTables.buttons.min.js" type="text/javascript"></script>
+    <script src="plugin/js/buttons.print.min.js" type="text/javascript"></script>
     <script type="text/javascript" src="model/JS/script.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="plugin/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="plugin/css/buttons.dataTables.min.css">
     <link rel="stylesheet" href="view/be_CSS/style_xls.css">
     <link rel="stylesheet" href="view/fe_CSS/style_startseite.css">
     <link rel="stylesheet" href="view/fe_CSS/style_header.css">
