0ca9b10fb9836f1e8935ca9dfd08dcde04e1e65c
Andreas Codalonga <45843565+TheArled@users.noreply.github.com>
2022-05-26 15:06:41 +0200

    finished

--- view/template/fe_startseite.tpl.php
+++ view/template/fe_startseite.tpl.php
@@ -76,32 +76,31 @@
 
                 <div class="accordion js-accordion" id="accordion">
                     <?php foreach ($fuehrungen as $fuehrung) { ?>
+                        <?php if (($anzahl_teilnehmer[$fuehrung->getId()] === $fuehrung->getKapazitaet())) continue; ?>
                         <div class="accordion__item js-accordion-item fuehrung <?= $fuehrung->getFachrichtung_id(); ?>">
                             <div class="accordion-header js-accordion-header">
                                 <div class="uhrzeit"><?= $fuehrung->getUhrzeitWelformed(); ?> Uhr</div>
                                 <div class="lehrer"><?= $fuehrung->getFuehrungspersonen(); ?></div>
                                 <div class="kapazitaet"><?= ($anzahl_teilnehmer[$fuehrung->getId()]) ? $anzahl_teilnehmer[$fuehrung->getId()] : 0; ?>/<?= $fuehrung->getKapazitaet(); ?></div>
                             </div>
-                            <?php if (($anzahl_teilnehmer[$fuehrung->getId()] !== $fuehrung->getKapazitaet())) : ?>
-                                <div class="accordion-body js-accordion-body">
-                                    <div class="accordion-body__contents">
-                                        <form class="form_buchung" method="POST" action="index.php?aktion=fe_startseite&anmelden=1">
-                                            <label for="vorname">Vorname:</label>
-                                            <input type="text" name="vorname" class="anmeldeinputs" title="zB. Max" value="" onchange="formAusgefuellt(this)" /><br />
-                                            <label for="nachname">Nachname:</label>
-                                            <input type="text" name="nachname" class="anmeldeinputs" title="zB. Mustermann" value="" onchange="formAusgefuellt(this)" /><br />
-                                            <label for="telefon">Telefon:</label>
-                                            <input type="telefon" name="telefon" class="anmeldeinputs" title="zB. 339 123 4567" value="" onchange="formAusgefuellt(this)" /><br />
-                                            <label for="email">E-Mail:</label>
-                                            <input type="email" name="email" class="anmeldeinputs" title="zB. max.mustermann@gmail.com" value="" onchange="formAusgefuellt(this)" /><br />
-                                            <label for="anzahl">Personen:</label>
-                                            <input type="number" name="anzahl" class="anmeldeinputs" title="zB. 6" value="1" max="<?= $fuehrung->getKapazitaet() - $anzahlTeilnehmer; ?>" min="1" placeholder="1" onchange="formAusgefuellt(this)" />
-                                            <input type="text" name="fuehrung_id" class="anmeldeinputs" value="<?= $fuehrung->getId(); ?>" hidden />
-                                            <input type="submit" value="Anmelden" disabled="disabled" />
-                                        </form>
-                                    </div>
+                            <div class="accordion-body js-accordion-body">
+                                <div class="accordion-body__contents">
+                                    <form class="form_buchung" method="POST" action="index.php?aktion=fe_startseite&anmelden=1">
+                                        <label for="vorname">Vorname:</label>
+                                        <input type="text" name="vorname" class="anmeldeinputs" title="zB. Max" value="" onchange="formAusgefuellt(this)" /><br />
+                                        <label for="nachname">Nachname:</label>
+                                        <input type="text" name="nachname" class="anmeldeinputs" title="zB. Mustermann" value="" onchange="formAusgefuellt(this)" /><br />
+                                        <label for="telefon">Telefon:</label>
+                                        <input type="telefon" name="telefon" class="anmeldeinputs" title="zB. 339 123 4567" value="" onchange="formAusgefuellt(this)" /><br />
+                                        <label for="email">E-Mail:</label>
+                                        <input type="email" name="email" class="anmeldeinputs" title="zB. max.mustermann@gmail.com" value="" onchange="formAusgefuellt(this)" /><br />
+                                        <label for="anzahl">Personen:</label>
+                                        <input type="number" name="anzahl" class="anmeldeinputs" title="zB. 6" value="1" max="<?= $fuehrung->getKapazitaet() - $anzahlTeilnehmer; ?>" min="1" placeholder="1" onchange="formAusgefuellt(this)" />
+                                        <input type="text" name="fuehrung_id" class="anmeldeinputs" value="<?= $fuehrung->getId(); ?>" hidden />
+                                        <input type="submit" value="Anmelden" disabled="disabled" />
+                                    </form>
                                 </div>
-                            <?php endif; ?>
+                            </div>
                         </div>
                     <?php } ?>
 
