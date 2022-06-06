1113412225f7e62cfe4ab238411a878bfc705db4
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2022-06-06 16:55:47 +0200

    fix 6

--- view/template/be_alle_einstellungen.tpl.php
+++ view/template/be_alle_einstellungen.tpl.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-<base href="openday.tschaufer.it/" />
+<base href="/openday.tschaufer.it/" />
     <meta charset="utf-8" />
     <title>Verwaltung</title>
     <link rel="stylesheet" href="/view/fe_CSS/style_startseite.css" />
