9d374c1538c070dbffe783669ee57d8059bafe6a
Daniel18742069 <daniel.kienzl3107@berufsschule.bz>
2022-06-06 17:25:22 +0200

    localhost optimiert 2

--- view/template/be_alle_einstellungen.tpl.php
+++ view/template/be_alle_einstellungen.tpl.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="view/fe_CSS/style_header.css" />
     <link rel="stylesheet" href="view/fe_CSS/style_footer.css" />
     <link rel="stylesheet" href="view/be_CSS/style_alle_od.css" />
-    <script type="text/javascript" src="/model/JS/script.js"></script>
+    <script type="text/javascript" src="model/js/script.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js"></script>
 
     <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.png" />
--- view/template/test.tpl.php
+++ view/template/test.tpl.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="view/fe_CSS/style_header.css" />
     <link rel="stylesheet" href="view/fe_CSS/style_footer.css" />
     <link rel="stylesheet" href="view/fe_CSS/style_subfooter.css" />
-    <script type="text/javascript" src="/model/JS/script.js"></script>
+    <script type="text/javascript" src="model/js/script.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="model/captcha/src/disk/slidercaptcha.min.css" rel="stylesheet" />
     <link rel="shortcut icon" type="image/x-icon" href="view/img/favicon.png" />
