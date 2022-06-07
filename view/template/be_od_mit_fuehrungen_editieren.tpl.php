6b357039ca74c96f5ba50e797f01c891079713a0
CapBen <99887278+CapBen@users.noreply.github.com>
2022-06-07 14:24:41 +0200

    Revert "Merge branch 'Nils-1.0'"
    This reverts commit fe2aeafca80edd9a5a05d3fcb3e671f88c6c0bba, reversing
    changes made to cc15a305c3184546e23d189b5ce9f66e99bea13a.

--- view/template/be_od_mit_fuehrungen_editieren.tpl.php
+++ view/template/be_od_mit_fuehrungen_editieren.tpl.php
@@ -29,12 +29,12 @@
 
     <section id="wrapper-be">
 
-        <div style="margin: 2rem 0 5rem 0">
+        <div class="wrapper-fe_startseite">
 
             <form action="FuehrungEditieren/<?= $offenerTag->getID() ?>" method="post">
                 <input type="submit" value="Speichern" name="anmeldenButton" id="btn_anmelden">
                 <input type="button" class="drucken" value="Drucken" name="druckenButton" id="btn_drucken">
-                <input type="button" class="zurueck-admin-panel" onclick="location.href='AlleOpenDay'" value="zurück"/> <!-- onclick="location.href='index.php?aktion=be_alle_od' -->
+                <input type="button" class="zurueck-admin-panel" onclick="location.href='AlleOpenDay'" value="zurück"/>
 
                 <div class="accordion js-accordion" id="accordion">
                     <?php foreach ($fuehrungen as $key => $fuehrung) {
@@ -81,14 +81,12 @@
                                         <div>
                                             <?php
                                                 $d = $anmeldung->getDatum();
-                                                $da = new DateTime($d);
-                                                $datum = $da->format('d.m.Y H:i');
                                             ?>
 
                                             <p><b>Name: </b> <?= $anmeldung->getFullName() ?> </p>
                                             <p><b>Email: </b> <?= $anmeldung->getEmail() ?> </p>
                                             <p><b>Telefonnummer: </b> <?= $anmeldung->getTelefon() ?> </p>
-                                            <p><b>Datum: </b> <?= $datum ?> </p>
+                                            <p><b>Datum: </b> <?= $d ?> </p>
                                             <p><b>Anzahl: </b> <?= $anmeldung->getAnzahl() ?> </p>
                                             <a href="FuehrungEditieren/<?= $offenerTag->getID() ?>&delete=<?= $anmeldung->getToken() ?>">
                                             <button type="button" value="Button" id="btn_loesche">Löschen</button>
